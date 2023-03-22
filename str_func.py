441b30f7523376da0c2844bc4ac2d37b1a459583
Ann-Ryazanova <zai97@mail.ru>
2023-03-23 00:39:59 +0300

    Added new function and docstring

--- str_func.py
+++ str_func.py
@@ -1,6 +1,18 @@
 def string_with_big_letter(string):
+    """
+    Функция возвращает строку со всеми заглавными буквами
+    """
     return string.upper()
+
+
+def string_with_one_letter(string):
+    """
+    Функция, которая возвращает буквы каждого слова в строке
+    """
+    return string.title()
+
 
 string = input("Введите строку ")
 
 print(string_with_big_letter(string))
+print(string_with_one_letter(string))
